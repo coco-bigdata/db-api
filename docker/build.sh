cfeb566180717a259c7590a1c65ced15d61ea967
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-03-11 16:16:10 +0800

    update

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,8 @@ DIR=$(dirname "$0")/..
 ROOT_DIR=`cd "$DIR"; pwd`
 echo $ROOT_DIR
 cd $ROOT_DIR
-VERSION=$(mvn -q -DforceStdout -N org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version)
+# VERSION=$(mvn -q -DforceStdout -N org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version)
+VERSION=3.0.0
 echo $VERSION
 TAG=${TAG:-"$VERSION"}
 docker build --build-arg VERSION=$VERSION -t yiluxiangbei/db-api:$TAG "$ROOT_DIR"
--- docker/push.sh
+++ docker/push.sh
@@ -2,8 +2,11 @@
 
 DIR=$(dirname "$0")/..
 ROOT_DIR=`cd "$DIR"; pwd`
+echo $ROOT_DIR
 cd $ROOT_DIR
-VERSION=$(mvn -q -DforceStdout -N org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version)
+# VERSION=$(mvn -q -DforceStdout -N org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version)
+VERSION=3.0.0
+echo $VERSION
 TAG=${TAG:-"$VERSION"}
 
 docker push yiluxiangbei/db-api:$TAG
